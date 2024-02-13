49332832b37bc6d59f877be5cf6e073d61bd1ee4
ΔΗΜΗΤΡΗΣ <63073176+1KRS@users.noreply.github.com>
2024-02-13 21:35:05 +0100

    Create Title component

---
+++ src/components/Title.jsx
@@ -0,0 +1,11 @@
+// import styled from 'styled-components';
+
+const Title = () => {
+  return <h1>Investment Calculator</h1>;
+};
+
+// const TitleText = styled.h1`
+//   font-size: 1.5rem;
+// `;
+
+export default Title;
